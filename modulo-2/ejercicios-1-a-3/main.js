0da320a28e25d29ec9bcab709db30e39b420d2b5
IsaGarabana <isinhagb@gmail.com>
2021-11-29 16:32:36 +0100

    add exercises 1 to 3

---
+++ modulo-2/ejercicios-1-a-3/main.js
@@ -0,0 +1,10 @@
+'use strict'; 
+document.querySelector('h1').innerHTML = 'ERROR 404';
+
+document.querySelector('p').innerHTML = 'Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente.';
+
+const titleElement = document.querySelector('.hola')
+titleElement.innerHTML = titleElement.innerHTML + ' qué tal estás'
+
+let miCasa = 'Riscos de Polanco';
+miCasa = 'Calle Sangenjo';
