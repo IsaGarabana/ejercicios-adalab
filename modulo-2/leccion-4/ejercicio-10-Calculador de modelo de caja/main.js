e8556f0085226e475f035f4ca9f4172b163d8253
IsaGarabana <isinhagb@gmail.com>
2021-12-08 13:58:49 +0100

    add new lesson 4 exercises

--- modulo-2/leccion-4/ejercicio-10-Calculador de modelo de caja/main.js
+++ modulo-2/leccion-4/ejercicio-10-Calculador de modelo de caja/main.js
@@ -9,7 +9,56 @@
 // el cuarto será un número con el border-size.
 // Para probar que funciona, ejecuta la función recogiendo el resultado en una variable e imprímela en la consola.
 
-function boxSize(boxType, boxWidth, padding, border) {
+// function boxSize(boxType, boxWidth, padding, border) {
+// 	if (boxType === "border-box") {
+// 		return `El ancho del contenido es ${
+// 			boxWidth - padding * 2 - border * 2
+// 		} px y el ancho total de la caja es ${boxWidth} px.`;
+// 	} else if (boxType === "content-box") {
+// 		return `El ancho del contenido es ${boxWidth} px y el ancho total de la caja es ${
+// 			boxWidth + padding * 2 + border * 2
+// 		} px.`;
+// 	} else {
+// 		return "Por favor comprueba que has introducido bien los parámetros";
+// 	}
+// }
+
+// const myBoxA = boxSize("border-box", 100, 10, 5);
+// console.log(myBoxA);
+// const myBoxB = boxSize("content-box", 100, 10, 5);
+// console.log(myBoxB);
+// const myBoxWrong = boxSize("connt-box", 100, 10, 5);
+// console.log(myBoxWrong);
+
+//Escrita con Switch
+
+// function boxSize(boxType, boxWidth, padding, border) {
+// 	switch (boxType) {
+// 		case "border-box":
+// 			return `El ancho del contenido es ${
+// 				boxWidth - padding * 2 - border * 2
+// 			} px y el ancho total de la caja es ${boxWidth} px.`;
+// 			break;
+// 		case "content-box":
+// 			return `El ancho del contenido es ${boxWidth} px y el ancho total de la caja es ${
+// 				boxWidth + padding * 2 + border * 2
+// 			} px.`;
+// 			break;
+// 		default:
+// 			return "Por favor comprueba que has introducido bien los parámetros";
+// 	}
+// }
+
+// const myBoxA = boxSize("border-box", 100, 10, 5);
+// console.log(myBoxA);
+// const myBoxB = boxSize("content-box", 100, 10, 5);
+// console.log(myBoxB);
+// const myBoxWrong = boxSize("connt-box", 100, 10, 5);
+// console.log(myBoxWrong);
+
+// Escrita como arrow
+
+const boxSize = (boxType, boxWidth, padding, border) => {
 	if (boxType === "border-box") {
 		return `El ancho del contenido es ${
 			boxWidth - padding * 2 - border * 2
@@ -21,7 +70,11 @@ function boxSize(boxType, boxWidth, padding, border) {
 	} else {
 		return "Por favor comprueba que has introducido bien los parámetros";
 	}
-}
+};
 
-const myBox = boxSize("content-box", 100, 10, 5);
-console.log(myBox);
+const myBoxA = boxSize("border-box", 150, 10, 5);
+console.log(myBoxA);
+const myBoxB = boxSize("content-box", 100, 10, 5);
+console.log(myBoxB);
+const myBoxWrong = boxSize("connt-box", 100, 10, 5);
+console.log(myBoxWrong);
