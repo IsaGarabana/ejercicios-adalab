dec68d28964a58346632529d05a8406695c7ccce
IsaGarabana <isinhagb@gmail.com>
2021-12-09 14:00:37 +0100

    add new exercises

--- modulo-2/leccion-5/ejercicio-03-Dame Ipsum/main.js
+++ modulo-2/leccion-5/ejercicio-03-Dame Ipsum/main.js
@@ -4,4 +4,12 @@
 const text = document.querySelector(".text");
 const text2 = document.querySelector(".text2");
 
-text.addEventListener("mouseover", () => text2.classList.remove("hidden"));
+// text.addEventListener("mouseover", () => text2.classList.remove("hidden"));
+
+//TEXT2.INNERPARA QUE SE AÑADAN PARRAFOS INDEFINIDAMENTE
+function handlerMouse() {
+	console.log("me pasó el mouse"); //verifica que la función ejecuta bien
+	text2.innerHTML += "<p> Lorem Ipsum </p>";
+}
+
+text.addEventListener("mouseover", handlerMouse);
