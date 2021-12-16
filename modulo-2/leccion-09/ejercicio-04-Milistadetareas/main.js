82ba158077b39aadfaeffaeac94d72656783d326
IsaGarabana <isinhagb@gmail.com>
2021-12-16 14:31:19 +0100

    add new exercises

---
+++ modulo-2/leccion-09/ejercicio-04-Milistadetareas/main.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const tasks = [
+	{ name: "Recoger setas en el campo", completed: true },
+	{ name: "Comprar pilas", completed: true },
+	{ name: "Poner una lavadora de blancos", completed: true },
+	{
+		name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+		completed: false,
+	},
+];
+
+const main = document.querySelector(".js_list");
+for (let index = 0; index < tasks.length; index++) {
+	if (data.completed === true) {
+		main.innerHTML += `<li class="completed"><input class="checkbox"  id=${index} type="checkbox" name="checkbox" checked
+		/> ${data.name}  </li>`;
+	} else {
+		main.innerHTML += `<li class=""><input class="checkbox" id=${index} type="checkbox" name="checkbox" 
+		/> ${data.name}  </li>`;
+	}
+}
+
+// for (const data of tasks) {
+// 	if (data.completed === true) {
+// 		main.innerHTML += `<li class="completed"><input class="checkbox"   type="checkbox" name="checkbox" checked
+// 		/> ${data.name}  </li>`;
+// 	} else {
+// 		main.innerHTML += `<li class=""><input class="checkbox" type="checkbox" name="checkbox"
+// 		/> ${data.name}  </li>`;
+// 	}
+// }
+
+const checkbox = document.querySelectorAll(".checkbox");
+
+// FUNCION HANDLE
+function handleClickOnCheckbox(event) {
+	if (event.currentTarget.checked === true) {
+		event.currentTarget.parentNode.classList.toggle("completed");
+	} else {
+		event.currentTarget.parentNode.classList.remove("completed");
+	}
+}
+
+//FUNCION LISTENER (2 OPCIONES)
+
+// for (let index = 0; index < checkbox.length; index++) {
+// 	checkbox[index].addEventListener("click", handleClickOnCheckbox);
+// }
+
+for (const eachCheckBox of checkbox) {
+	eachCheckBox.addEventListener("click", handleClickOnCheckbox);
+}
